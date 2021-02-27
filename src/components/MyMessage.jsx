93cab028eb3bfd05dde077bd766a017b9247d900
vicky24111999 <60955800+vicky24111999@users.noreply.github.com>
2021-02-27 21:29:33 +0530

    my chat web

---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,26 @@
+const MyMessage = ({ message }) => {
+  if (message?.attachments?.length > 0) {
+    return (
+      <img
+        src={message.attachments[0].file}
+        alt="message-attachments"
+        className="message-image"
+        style={{ float: "right" }}
+      />
+    );
+  }
+  return (
+    <div
+      className="message"
+      style={{
+        float: "right",
+        marginRight: "18px",
+        color: "white",
+        backgroundColor: "#382A50",
+      }}
+    >
+      {message.text}
+    </div>
+  );
+};
+export default MyMessage;
